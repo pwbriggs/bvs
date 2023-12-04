8167f5ac3835debbef763807c9e850cd3a1b4baf
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2023-12-03 21:50:50 -0800

    feat: add error boundary
    Add a root Remix ErrorBoundary, with some simple inline styling (because this acts as the default 404 message, so it needs to look nice).

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,15 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import {
+    isRouteErrorResponse,
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
+    useRouteError,
+} from "@remix-run/react";
 
 export const links: LinksFunction = () => [
     ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
@@ -22,5 +31,44 @@ export default function App() {
         <LiveReload/>
         </body>
         </html>
+    );
+}
+
+export function ErrorBoundary() {
+    const error = useRouteError();
+    let message;
+
+    if (isRouteErrorResponse(error)) {
+        message = (
+            <>
+                <h1>
+                    {error.status} {error.statusText}
+                </h1>
+                <p>{error.data}</p>
+            </>
+        );
+    } else if (error instanceof Error) {
+        message = (
+            <>
+                <h1>Error</h1>
+                <p>{error.message}</p>
+                <p>The stack trace is:</p>
+                <pre>{error.stack}</pre>
+            </>
+        );
+    } else {
+        message = <h1>Unknown Error</h1>;
+    }
+    return (
+        <div style={{
+            backgroundColor: "#fce4e4",
+            fontFamily: '"Segoe UI", sans-serif',
+            color: "darkred",
+            margin: "1em",
+            padding: "2rem",
+            borderRadius: "1em",
+        }}>
+            {message}
+        </div>
     );
 }
