cd49438365a507ce27a815d8161b9a3778d9a1f9
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2023-12-03 20:40:55 -0800

    refactor: code style
    Run code reformat and add settings to IDE config.

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ![GitHub commit activity (branch)](https://img.shields.io/github/commit-activity/m/pwbriggs/bvs)
 
-A web app for scheduling volunteers. Built on [React](https://react.dev/) and [Remix](https://remix.run/). Currently under initial active development,
+A web app for scheduling volunteers. Built on [React](https://react.dev/) and [Remix](https://remix.run/). Currently
+under initial active development,
 ![GitHub last commit (branch)](https://img.shields.io/github/last-commit/pwbriggs/bvs/main).
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 import * as url from "node:url";
 
-import { createRequestHandler } from "@remix-run/express";
-import { broadcastDevReady, installGlobals } from "@remix-run/node";
+import {createRequestHandler} from "@remix-run/express";
+import {broadcastDevReady, installGlobals} from "@remix-run/node";
 import compression from "compression";
 import express from "express";
 import morgan from "morgan";
@@ -19,12 +19,12 @@ const VERSION_PATH = path.resolve("build/version.txt");
 
 const initialBuild = await reimportServer();
 const remixHandler =
-  process.env.NODE_ENV === "development"
-    ? await createDevRequestHandler(initialBuild)
-    : createRequestHandler({
-        build: initialBuild,
-        mode: initialBuild.mode,
-      });
+    process.env.NODE_ENV === "development"
+        ? await createDevRequestHandler(initialBuild)
+        : createRequestHandler({
+            build: initialBuild,
+            mode: initialBuild.mode,
+        });
 
 const app = express();
 
@@ -35,13 +35,13 @@ app.disable("x-powered-by");
 
 // Remix fingerprints its assets so we can cache forever.
 app.use(
-  "/build",
-  express.static("public/build", { immutable: true, maxAge: "1y" })
+    "/build",
+    express.static("public/build", {immutable: true, maxAge: "1y"})
 );
 
 // Everything else (like favicon.ico) is cached for an hour. You may want to be
 // more aggressive with this caching.
-app.use(express.static("public", { maxAge: "1h" }));
+app.use(express.static("public", {maxAge: "1h"}));
 
 app.use(morgan("tiny"));
 
@@ -49,24 +49,24 @@ app.all("*", remixHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, async () => {
-  console.log(`Express server listening on port ${port}`);
+    console.log(`Express server listening on port ${port}`);
 
-  if (process.env.NODE_ENV === "development") {
-    broadcastDevReady(initialBuild);
-  }
+    if (process.env.NODE_ENV === "development") {
+        broadcastDevReady(initialBuild);
+    }
 });
 
 /**
  * @returns {Promise<ServerBuild>}
  */
 async function reimportServer() {
-  const stat = fs.statSync(BUILD_PATH);
+    const stat = fs.statSync(BUILD_PATH);
 
-  // convert build path to URL for Windows compatibility with dynamic `import`
-  const BUILD_URL = url.pathToFileURL(BUILD_PATH).href;
+    // convert build path to URL for Windows compatibility with dynamic `import`
+    const BUILD_URL = url.pathToFileURL(BUILD_PATH).href;
 
-  // use a timestamp query parameter to bust the import cache
-  return import(BUILD_URL + "?t=" + stat.mtimeMs);
+    // use a timestamp query parameter to bust the import cache
+    return import(BUILD_URL + "?t=" + stat.mtimeMs);
 }
 
 /**
@@ -74,28 +74,30 @@ async function reimportServer() {
  * @returns {Promise<import('@remix-run/express').RequestHandler>}
  */
 async function createDevRequestHandler(initialBuild) {
-  let build = initialBuild;
-  async function handleServerUpdate() {
-    // 1. re-import the server build
-    build = await reimportServer();
-    // 2. tell Remix that this app server is now up-to-date and ready
-    broadcastDevReady(build);
-  }
-  const chokidar = await import("chokidar");
-  chokidar
-    .watch(VERSION_PATH, { ignoreInitial: true })
-    .on("add", handleServerUpdate)
-    .on("change", handleServerUpdate);
+    let build = initialBuild;
 
-  // wrap request handler to make sure its recreated with the latest build for every request
-  return async (req, res, next) => {
-    try {
-      return createRequestHandler({
-        build,
-        mode: "development",
-      })(req, res, next);
-    } catch (error) {
-      next(error);
+    async function handleServerUpdate() {
+        // 1. re-import the server build
+        build = await reimportServer();
+        // 2. tell Remix that this app server is now up-to-date and ready
+        broadcastDevReady(build);
     }
-  };
+
+    const chokidar = await import("chokidar");
+    chokidar
+        .watch(VERSION_PATH, {ignoreInitial: true})
+        .on("add", handleServerUpdate)
+        .on("change", handleServerUpdate);
+
+    // wrap request handler to make sure its recreated with the latest build for every request
+    return async (req, res, next) => {
+        try {
+            return createRequestHandler({
+                build,
+                mode: "development",
+            })(req, res, next);
+        } catch (error) {
+            next(error);
+        }
+    };
 }
