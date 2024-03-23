3a53cc2ae786f6915f674972d985758c3fdfb4a0
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2024-03-22 22:42:38 -0700

    fix: :ambulance: add missing prisma imports
    Add missing imports of prisma, which exists as a global in development only.

--- app/scripts/accounts.server.ts
+++ app/scripts/accounts.server.ts
@@ -1,3 +1,5 @@
+import { prisma } from "~/scripts/prisma.server";
+
 export async function userExists(username: string) {
     return await prisma.user.findUnique({ where: { username } }) !== null;
 }
