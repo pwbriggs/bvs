a3912fa21f07ce9d495bfac2683730ab7c15c29e
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2024-05-11 09:33:26 -0700

    test: :white_check_mark: expect unauthenticated requests to My Schedule to redirect
    Add a test that expects unauthenticated requests to My Schedule (/my-schedule) to redirect to the login page.

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -4,4 +4,9 @@ test.describe("Auth system", () => {
     test('has actual tests', async ({ page }) => {
         await expect(false).toBeTruthy();
     });
+
+    test("redirects unauthenticated requests to My Schedule", async ({ page }) => {
+        await page.goto("/my-schedule");
+        await expect(page).toHaveURL("/login?next=/my-schedule");
+    });
 });
